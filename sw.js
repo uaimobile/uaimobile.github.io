bd7c4eb8b8ad870e290fdd9afde02220f92aefaf
uaimobile <54996175+uaimobile@users.noreply.github.com>
2019-09-06 14:40:31 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+
+const cacheActual = 'UAIMobile-v1';
+
+const recursosEstaticos = [
+  'css/materialize.min.css',
+  'css/estilos.css',
+  'js/materialize.min.js' , 
+  'icons/pdf.svg'
+];
+
+self.addEventListener('install', function(event) 
+{
+  event.waitUntil
+  (
+    caches.open(cacheActual).then(function(cache) 
+       {
+        return cache.addAll(recursosEstaticos);
+       })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cachesExistentes) {
+      return Promise.all(
+        cachesExistentes.map(function(cacheVieja) {
+          if (cacheVieja !== cacheActual) 	  
+		  {		
+            return caches.delete(cacheVieja);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim(); //fuerza que todos los clientes se actualicen
+});
