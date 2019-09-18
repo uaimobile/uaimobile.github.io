82c0ab7a3ee5deabfb04794a2b95fd4259d798c8
uaimobile <54996175+uaimobile@users.noreply.github.com>
2019-09-18 19:10:00 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const cacheActual = 'UAIMobile-v7';
+const cacheActual = 'UAIMobile-v8';
 
 const recursosEstaticos = [
   'css/materialize.min.css',
@@ -21,6 +21,24 @@ self.addEventListener('install', function(event)
   );
 });
 
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    // Try the cache
+    caches.match(event.request).then(function(response) {
+      // Fall back to network
+      return response || fetch(event.request);
+    }).catch(function() {
+      // If both fail, show a generic fallback:
+      return caches.match('/offline.html');
+      // However, in reality you'd have many different
+      // fallbacks, depending on URL & headers.
+      // Eg, a fallback silhouette image for avatars.
+    })
+  );
+});
+
+/*
 self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
@@ -47,7 +65,7 @@ self.addEventListener('fetch', function (event) {
   );
 });
 
-	  
+*/	  
 	  
 
 /*
