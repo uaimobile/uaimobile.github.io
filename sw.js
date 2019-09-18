dda6f75f675d72950335195970508c01e082bdc9
uaimobile <54996175+uaimobile@users.noreply.github.com>
2019-09-18 19:27:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,12 @@
 
-const cacheActual = 'UAIMobile-v8';
+const cacheActual = 'UAIMobile-v9';
 
 const recursosEstaticos = [
   'css/materialize.min.css',
   'css/estilos.css',
   'js/materialize.min.js' , 
   'icons/uai.svg',
-  'icons/192.png',
-   'offline.html'
+  'icons/192.png'   
 ];
 
 self.addEventListener('install', function(event) 
@@ -19,64 +18,8 @@ self.addEventListener('install', function(event)
         return cache.addAll(recursosEstaticos);
        })
   );
-});
-
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    // Try the cache
-    caches.match(event.request).then(function(response) {
-      // Fall back to network
-      return response || fetch(event.request);
-    }).catch(function() {
-      // If both fail, show a generic fallback:
-      return caches.match('/offline.html');
-      // However, in reality you'd have many different
-      // fallbacks, depending on URL & headers.
-      // Eg, a fallback silhouette image for avatars.
-    })
-  );
-});
-
-/*
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        
-	      
-	if (event.request.mode === 'navigate') {
- 	 try 
-	    {	   
-	      	return  fetch(request);
-	    } 
-	    catch (err) 
-	    {	    
-		return caches.match('offline.html');	    	 
-   	    }	  
-	}
-	      
-	
-	      
-      })
-  );
-});
-
-*/	  
+});	 
 	  
-
-/*
-try 
-	    {	   
-	      	return  fetch(request);
-	    } 
-	    catch (err) 
-	    {	    
-		return caches.match('offline.html');	    	 
-   	    }	  
 
 self.addEventListener('fetch', function (event) {
   event.respondWith(
@@ -90,4 +33,3 @@ self.addEventListener('fetch', function (event) {
   );
 });
 
-*/
